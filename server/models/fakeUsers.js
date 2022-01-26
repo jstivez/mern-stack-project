6786e17fa82201a25d76105e714043da5b1a9159
wtphillips <wtphillips96@gmail.com>
2022-01-26 18:09:45 -0500

    name changed in user profile

--- server/models/fakeUsers.js
+++ server/models/fakeUsers.js
@@ -1,7 +1,7 @@
 const allUsers = {
         users:  {
                 num: "0001",
-                name: "jon",
+                name: "Post-It Forever",
                 age: "25",
                 location: "lexington",
                 height: "6"
